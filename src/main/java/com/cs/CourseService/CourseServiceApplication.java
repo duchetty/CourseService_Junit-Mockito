8fd9ade1743529c73d765304697aba951ca13199
duchetty <duchettydevendra@gmail.com>
2025-03-10 16:45:35 +0530

    main class modified

--- src/main/java/com/cs/CourseService/CourseServiceApplication.java
+++ src/main/java/com/cs/CourseService/CourseServiceApplication.java
@@ -8,6 +8,7 @@ public class CourseServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseServiceApplication.class, args);
+		System.out.println("Application Test cases are passed");
 	}
 
 }
